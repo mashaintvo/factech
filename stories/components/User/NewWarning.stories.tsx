8fcf8f4a7f4741c1e98e20c5eb0c7182c37aa5b1
María Alejandra Sanjuán <marialeja0314@gmail.com>
2025-11-08 12:08:45 -0500

    create NewWarning component

--- stories/components/User/NewWarning.stories.tsx
+++ stories/components/User/NewWarning.stories.tsx
@@ -1,19 +1,73 @@
+import React, { useState } from 'react';
 import { Meta, StoryObj } from '@storybook/react';
 
-const NewWarning = () => {
+type WarningProps = {
+    title?: string;
+    message?: string;
+    buttonText?: string;
+    onClose?: () => void;
+};
+
+const NewWarning: React.FC<WarningProps> = ({
+    title = 'Warning!',
+    message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    buttonText = 'OK',
+    onClose,
+}) => {
+    const handleOverlayClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        if (e.target === e.currentTarget && onClose) {
+            onClose();
+        }
+    };
+
     return (
-        <>
-            <h1>Hi, put here you warning component code</h1>
-        </>
+        <div className="o-warning-overlay" onClick={handleOverlayClick}>
+            <div className="o-warning-block">
+                <div className="o-warning-block__icon">
+                    <img
+                        src="/assets/images/icons/icon--warning.svg"
+                        alt="Warning icon"
+                        className="o-warning-block__icon-img"
+                    />
+                </div>
+
+                <button className="o-warning-block__close" aria-label="Close warning" onClick={onClose}>
+                    ✕
+                </button>
+                <h1 className="o-warning-block__title a-text-style-1">{title}</h1>
+                <div className="o-warning-block__message">
+                    <p className="a-text-note-grey-3">{message}</p>
+                    <button className="o-warning-block__button">{buttonText}</button>
+                </div>
+            </div>
+        </div>
     );
 };
 
-const meta = {
+const meta: Meta<typeof NewWarning> = {
+    title: 'Components/User/NewWarning',
     component: NewWarning,
     tags: ['autodocs'],
-} satisfies Meta;
+};
 
 export default meta;
-type Story = StoryObj<typeof meta>;
+type Story = StoryObj<typeof NewWarning>;
 
-export const Default: Story = {};
+export const Default: Story = {
+    render: () => {
+        const [isOpen, setIsOpen] = useState(true);
+        return (
+            <>
+                {isOpen && (
+                    <NewWarning
+                        title="Warning!"
+                        message="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt."
+                        buttonText="OKE"
+                        onClose={() => setIsOpen(false)}
+                    />
+                )}
+                <button onClick={() => setIsOpen(true)}>Mostrar warning</button>
+            </>
+        );
+    },
+};
